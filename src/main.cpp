e6b2d03f367e20ea3836f17a59e83fa08a589a7f
Eric Zhang <ericbkzhang@gmail.com>
2024-08-21 18:50:18 -0400

    Implement basic file reading and writing

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,28 @@
 #include <fstream>
 using namespace std;
 
+string readFile(string filePath)
+{
+    ifstream inputFile(filePath);
+
+    string fileString;
+    while (getline(inputFile, fileString))
+    inputFile.close();
+
+    return fileString;
+}
+
 int main()
 {
-    // Create and read a file.
-    ifstream InputFile("input.txt");
+    string plaintext = readFile("./plaintext/sample.txt");
+    std::cout << "Plaintext:\n" << plaintext << "\n\n";
 
-    string plaintext;
+    string key = readFile("./keys/sample.txt");
+    std::cout << "Key:\n" << key;
 
-    while (getline(InputFile, plaintext))
-    {
-    }
-
-    cout << plaintext;
-
-    InputFile.close();
+    string ciphertext = plaintext;
+    ofstream outputFile("./ciphertext/sample.txt");
+    outputFile << ciphertext;
 
     return 0;
 }
