bff510ac13fa27b01e82177681a4952e43e382fa
Eric Zhang <ericbkzhang@gmail.com>
2024-08-24 18:53:39 -0400

    Add functions to encrypt and decrypt

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ An implementation of Vigenère Cipher using C++.
 # Plaintext
 > [!Caution]
 > This cipher is only designed to handle ASCII characters.
-> Attempting to use non-ASCII will not work properly.
+> Attempting to use non-ASCII characters will not work properly.
--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,60 @@
 #include <fstream>
 #include <iostream>
 
+/**
+ * @brief Encrypt a single character using Vigenère's cipher and a character as the key.
+ *
+ * Uses 7-bit ASCII values to encrypt the character.
+ * Avoids producing a ASCII control character.
+ *
+ * @param plaintext A single character to use as the plaintext.
+ * @param key A single character to use as the key.
+ * @return char A ciphertext character produced using the key and plaintext.
+ *         Will not be a control character.
+ */
+char encryptChar(char plaintext, char key)
+{
+    // Apply Vigenère's cipher.
+    // Subtract 32 from the modulus to account for non-printable ASCII characters,
+    // and add 32 to the result to prevent non-printable ASCII characters.
+    return (char)(((int)plaintext + (int)key) % (std::numeric_limits<char>::max() - 32) + 32);
+}
+
+/**
+ * @brief Decrypt a single character using Vigenère's cipher and a character as the key.
+ *
+ * Uses 7-bit ASCII values to decrypt the character.
+ * Avoids producing a ASCII control character.
+ *
+ * @param ciphertext A single character to use as the ciphertext.
+ * @param key A single character to use as the key.
+ * @return char A plaintext character produced using the key and plaintext.
+ *         Will not be a control character.
+ */
+char decryptChar(char ciphertext, char key)
+{
+    // Undo Vigenère's cipher.
+    // Subtract 32 from the modulus to account for non-printable ASCII characters,
+    // and subtract 32 from the result to prevent non-printable ASCII characters.
+    return (char)(((int)ciphertext - (int)key) % (std::numeric_limits<char>::max() - 32) - 32);
+}
+
 int main(int, char *[])
 {
     // Raise an error if the user did not pass the necessary arguments.
-    if (__argc < 2)
+    if (__argc < 3)
     {
-        throw std::invalid_argument("Please pass in a file name.");
+        throw std::invalid_argument("Please pass in an operation(\"encrypt\" or \"decrypt\") and file name.");
     }
 
-    std::string fileName = (std::string)__argv[1];
+    std::string fileName = (std::string)__argv[2];
 
     // Raise an error if the plaintext and key differ in length.
     int plaintextSize = std::filesystem::file_size("./plaintext/" + fileName);
     int keySize = std::filesystem::file_size("./keys/" + fileName);
     if (plaintextSize != keySize)
     {
-        throw std::invalid_argument("The plaintext and key must be of the same length.");
+        throw std::invalid_argument("The input and key must be of the same length.");
     }
 
     std::ifstream plaintextFile("./plaintext/" + fileName);
@@ -36,12 +74,12 @@ int main(int, char *[])
         plaintext += plaintextChar;
 
         char keyChar = keyFile.get();
-        key += keyChar;        
+        key += keyChar;
 
         // Apply Vigenère's cipher.
         // Subtract 32 from the modulus to account for non-printable ASCII characters,
         // and add 32 to the result to prevent non-printable ASCII characters.
-        char ciphertextChar = (char)(((int)plaintextChar + (int)keyChar) % (std::numeric_limits<char>::max() - 32) + 32);
+        char ciphertextChar = encryptChar(plaintextChar, keyChar);
         ciphertext += ciphertextChar;
     }
     ciphertextFile << ciphertext;
